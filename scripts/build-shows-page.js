a0436c1f7f4a0110e4e4c3feb7c97deafbaa15f9
krazzington <mkrazbookings@gmail.com>
2022-04-17 23:01:37 -0400

    final commit, valient effort\? it was an effort

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,19 +1,7 @@
-// The individual rows of the Shows table need to have a hover state applied to them when a cursor is hovering over the table 
-// row, as per style guide. This can be done by utilizing a pseudo class within your Sass.
-// Additionally, clicking on an individual row should make that row "selected" or "active", applying a modifier
-// CSS class via JavaScript. Utilize your knowledge of both JavaScript and Sass to accomplish this.
-
-
-
-
 const ticketbutton = document.createElement( 'div' );
 ticketbutton.classList.add('shows__showRow--ticketbutton')
 
 const gigDetailsAPI = 'https://project-1-api.herokuapp.com/showdates?api_key=b286c6ec-3de5-4598-9517-5a9617d60bd9';
-
-// let axiosGet = axios.get(gigDetailsAPI).then(response => {
-//     console.log(response.data);
-//   });
 
   const showDivGenerator = ( showRows ) => {
     const dateInt = parseInt(showRows.date);
@@ -26,8 +14,8 @@ const gigDetailsAPI = 'https://project-1-api.herokuapp.com/showdates?api_key=b28
     newShow.classList.add( 'shows__showDiv' );
 
     //DATE DIVS
-    const dateDiv = document.createElement( 'div' ); // date div in the show div --- this should own the header and date info
-    dateDiv.classList.add( 'shows__dateDiv' ); // this adds the class to the date info
+    const dateDiv = document.createElement( 'div' ); 
+    dateDiv.classList.add( 'shows__dateDiv' ); 
     const dateHeaderDiv = document.createElement( 'div' );
     dateHeaderDiv.classList.add( 'shows__divHeaders', 'divHeaders' );
     dateHeaderDiv.innerText = 'date';
@@ -38,8 +26,8 @@ const gigDetailsAPI = 'https://project-1-api.herokuapp.com/showdates?api_key=b28
     dateDiv.appendChild( dateInfoDiv );
     
     //VENUE DIVS
-    const venueDiv = document.createElement( 'div' ); // date div in the show div --- this should own the header and date info
-    venueDiv.classList.add( 'shows__dateDiv' ); // this adds the class to the date info
+    const venueDiv = document.createElement( 'div' ); 
+    venueDiv.classList.add( 'shows__venueDiv' );
     const venueHeaderDiv = document.createElement( 'div' );
     venueHeaderDiv.classList.add( 'shows__divHeaders' );
     venueHeaderDiv.innerText = 'venue';
@@ -50,8 +38,8 @@ const gigDetailsAPI = 'https://project-1-api.herokuapp.com/showdates?api_key=b28
     venueDiv.appendChild( venueInfoDiv );
 
     //LOCATION DIVS
-    const locationDiv = document.createElement( 'div' ); // date div in the show div --- this should own the header and date info
-    locationDiv.classList.add( 'shows__locationDiv' ); // this adds the class to the date info
+    const locationDiv = document.createElement( 'div' );
+    locationDiv.classList.add( 'shows__locationDiv' );
     const locationHeaderDiv = document.createElement( 'div' );
     locationHeaderDiv.classList.add( 'shows__divHeaders' );
     locationHeaderDiv.innerText = 'location';
@@ -69,7 +57,7 @@ const gigDetailsAPI = 'https://project-1-api.herokuapp.com/showdates?api_key=b28
     ticketButtonHeaderDiv.innerText = '';
     const ticketButtonInfoDiv = document.createElement( 'div' );
     ticketButtonInfoDiv.innerText = 'buy tickets';
-    ticketButtonInfoDiv.classList.add( 'shows__ticketButtonDiv--ticketButtonInfo' );
+    ticketButtonInfoDiv.classList.add( 'shows__ticketButtonDiv--ticketButton' );
     ticketButtonDiv.appendChild( ticketButtonHeaderDiv );
     ticketButtonDiv.appendChild( ticketButtonInfoDiv );
 
@@ -97,22 +85,14 @@ const showIntoTheRow = (  ) => {
         createShowDiv.appendChild( showDivGenerator( gig ) );
         showDestination.appendChild( createShowDiv );
         } );
-    }).catch(document.write("An error has occurred"));
+    }).catch(console.log("An error has occurred"));
 };
 
 //Invoking my renderer
-
 showIntoTheRow( gigDetailsAPI );
 
-// querySelector(row);
-// addEventListener(click ,function)
-// onclick, add class
-// if()
-
-// the function needs to add a class
 
 //Active Class
-
 const activeClass = document.querySelectorAll( ".shows__showDiv" );
 
 const rowActivated = (show) => {
@@ -124,7 +104,3 @@ for(let i = 0; i < activeClass.length; i++){
     activeClass[i].addEventListener( 'click', function() { rowActivated(activeClass[i])})
 
 }
-// console.log(activeClass);
-
-// let showsSectionLog = document.getElementById( 'showTable' );
-// console.log(showsSectionLog);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,27 +1,13 @@
-//must have a function called displayComment() that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
-
-//No template literals should be used. All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
-
-// You must use an HTML Form with the following functionality:
-// That submits using the addEventListener
-// Prevents the page from reloading when submitting a new comment
-// Constructs a new comment object
-// Pushes a new comment object to an array of comments
-// Clears all comments from the page
-// Re-renders to the page all comments from the comment array
-// Clears the input fields after submitting a new comment
 const formsSection = document.getElementById( 'formsSection' );
 
 const commentsAPI = 'https://project-1-api.herokuapp.com/comments?api_key=55b535b7-04a6-4cf7-bc79-32506c08e2cf';
 
-
-
-// let axiosGet = axios.get(commentsAPI).then(response => {
-//   console.log(response.data);
-// });
-
 //creates the div for the comments section - commentObject is a placeholder
 const displayComment = ( commentObject ) => {
+    const dateInt = parseInt(commentObject.timestamp);
+    const d = new Date(dateInt);
+    const date = d.toLocaleString("en-US", {month:"numeric", day: "numeric", year: "numeric"});
+
     //creates the div for the above function and gives it a class
     let listItem = document.createElement( 'div' );
     listItem.classList.add( 'postedCommentSection' );
@@ -50,7 +36,7 @@ const displayComment = ( commentObject ) => {
     nameNode.innerText = commentObject.name;
     //plugs the text into the date nodes from the called function
 
-    dateNode.innerText = commentObject.timestamp;
+    dateNode.innerText = date;
     //plugs the comment into the comment nodes from the called function
     commentNode.innerText = commentObject.comment;
 
@@ -80,20 +66,12 @@ let commentRender = ( comments ) => {
     console.log(response);
     const array = response.data;
 
-array.sort((a, b) => b.timestamp - a.timestamp)
+    array.sort((a, b) => b.timestamp - a.timestamp)
 
-for (let i = 0; 1 < array.length; i++) {
+    for (let i = 0; 1 < array.length; i++) {
 	let commentObject = array[i];
 	unorderedList.appendChild( displayComment ( commentObject ));
-}
-
-    //loops through response.data retreived from commentAPI
-    // for (let i = 0; i < response.data.length; i++) {
-        
-    //     //commentObject refers to the first function (displayComment) and
-    //     let commentObject = response.data[i];
-    //     unorderedList.appendChild( displayComment( commentObject ) );
-    //     }
+    }
     });
 }
 
@@ -113,26 +91,15 @@ const addComment = ( event ) => {
 
     const commentObject = {
         name: name,
-        //date: todaysDate,
         comment: comment,
     }
 
-    
     axios.post(commentsAPI, commentObject )
-.then((res) => console.log('res', res))
-.catch((err) => console.log('error', err));
+    .then((res) => console.log('res', res))
+    .catch((err) => console.log('error', err));
 
-
-    //commentObject.classList.add('postedCommentSection__mainDiv--commentNode');
-    // const axiosPost = axios.post(commentsAPI);
-    // axiosPost( commentObject ).then((response) => {
-    //     console.log(response);
-    // }).catch((error) => console.log(error));//----------------------------------needs to post to API, then call the next two functions
-    // ;
-    // console.log(axiosPost);
     unorderedList.innerHTML = ''; 
     commentRender ( axiosPost );
-
 }
 
 commentRender( commentsAPI );
